ff9b68f84c9beb777d8a7918ab46953492b53932
Duncan Beevers <duncanbeevers@gmail.com>
2008-05-12 16:57:45 -0700

    Require lib in init.rb, plugins don't work without this.  Make find_random_ids class method, oversight.

--- lib/find_random.rb
+++ lib/find_random.rb
@@ -1,5 +1,5 @@
 class ActiveRecord::Base
-  def self.find_first_random
+  def self.find_one_random
     find(:first, :offset => rand(count))
   end
 
@@ -7,7 +7,7 @@ class ActiveRecord::Base
     find(find_random_ids)
   end
 
-  def find_random_ids how_many
+  def self.find_random_ids how_many
     ids = []
     all_ids = find(:all, :select => 'id').map do |e| e.id end
     how_many.times do

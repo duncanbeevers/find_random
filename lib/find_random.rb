1e16603c46f07ad00c5472ec1d63961aacb889d2
Duncan Beevers <duncanbeevers@gmail.com>
2008-10-23 10:37:43 -0700

    Use find_ids if available.

--- lib/find_random.rb
+++ lib/find_random.rb
@@ -8,11 +8,15 @@ class ActiveRecord::Base
   end
 
   def self.find_random_ids how_many
+    all_ids = respond_to?(:find_ids) ?
+      find_ids :
+      find(:all, :select => 'id').map { |e| e.id }
+    
     ids = []
-    all_ids = find(:all, :select => 'id').map do |e| e.id end
     how_many.times do
       ids << all_ids.delete_at(rand(all_ids.size))
     end
+    
     ids
   end
 end
